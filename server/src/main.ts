8b133bce9d7a40899e177e6058d02a5fbd1b2bad
girordo <tsgiroldo@gmail.com>
2022-06-13 18:00:00 -0300

    :sparkles: | Adding main function

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,18 @@
-async function main() {}
+import "reflect-metadata";
+import { createServer } from "./utils/createServer";
+
+async function main() {
+  const { app, server } = await createServer();
+
+  app.get("/healthcheck", async () => "OK");
+
+  await server.start();
+
+  await app.listen({
+    port: 4000,
+  });
+
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+}
 
 main();
--- server/src/modules/user/user.resolver.ts
+++ server/src/modules/user/user.resolver.ts
@@ -1,6 +1,16 @@
-import { Resolver } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
+import { User } from "./user.dto";
 
-@Resolver()
-class UserResolver {}
+@Resolver(() => User)
+class UserResolver {
+  @Query()
+  user() {
+    return {
+      id: "1",
+      username: "test",
+      email: "",
+    };
+  }
+}
 
 export default UserResolver;
